1a58825de0cabeacf90301828986129e339fb331
Jickay <sieunhan482@gmail.com>
2024-06-22 21:13:42 +0700

    Add files via upload

--- steam-clone-main/Pages/Home/main-js.js
+++ steam-clone-main/Pages/Home/main-js.js
@@ -19,25 +19,41 @@ function redirectToProductPage() {
   window.location.href = '../../Pages/Search/search.html';
 }
 
-let slideIndex = 0;
-const slides = document.querySelectorAll('.games');
-const totalSlides = slides.length;
-const slidesToShow = 19;
 
-function updateSlidePosition() {
-  const slider = document.getElementById('topGame');
-  const newTransform = slideIndex * 3.4; // 20% for each slide to show 5 at a time
-  slider.style.transform = `translateX(${newTransform}%)`;
-}
+  let slideGameForUIndex = 0;
+  let slideTopGameIndex = 0;
+  let slideIndex = 0;
 
-function moveSlides(n) {
-  slideIndex += n;
-  if (slideIndex < 0) {
-    slideIndex = 0;
-  } else if (slideIndex > totalSlides - slidesToShow) {
-    slideIndex = totalSlides - slidesToShow;
+  const slides = document.querySelectorAll('.games');
+  const totalSlides = slides.length;
+  const slidesToShow = 19;
+  let slideName;
+  function updateSlidePosition(name) {
+    const slider = document.getElementById(name);
+    const newTransform = slideIndex * 3.4; // 20% for each slide to show 5 at a time
+    slider.style.transform = `translateX(${newTransform}%)`;
   }
-  updateSlidePosition();
-}
-
-updateSlidePosition();
+  
+  function moveSlides(n, name) {
+    slideName = name;
+    switch (name) {
+      case 'gameForU':
+        slideIndex = slideGameForUIndex;
+        break;
+      case 'topGame':
+        slideIndex = slideTopGameIndex;
+        break;
+      default:
+        break;
+    }
+    slideIndex += n;
+    if (slideIndex < 0) {
+      slideIndex = 0;
+    } else if (slideIndex > totalSlides - slidesToShow) {
+      slideIndex = totalSlides - slidesToShow;
+    }
+    updateSlidePosition(name);
+    
+  }
+  
+  updateSlidePosition(slideName);
